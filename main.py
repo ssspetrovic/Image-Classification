6d14c80a4c4eb1f7b8976d5e26429a1f17e6bca3
Srdjan Petrovic <s.petrovic852@gmail.com>
2023-06-07 23:13:49 +0200

    Complete model

--- main.py
+++ main.py
@@ -1,13 +1,11 @@
 import numpy as np
 import matplotlib.pyplot as plt
-import tensorflow as tf
+from tensorflow import keras
 from keras.utils import np_utils
-from tensorflow.keras import datasets, layers, models
+from keras import datasets, layers, models
 
 (train_images, train_labels), (test_images, test_labels) = datasets.cifar10.load_data()
 
-# normalizing the RGB values of the images to be between the range of 0 and 1
-train_images, test_images = train_images / 255.0, test_images / 255.0
 
 class_names = [
     "airplane",
@@ -22,60 +20,76 @@ class_names = [
     "truck",
 ]
 
+
+print("\nPrimer jednog kanala ulazne slike:")
+print(train_images[0][0][:3])
+print("\nPrimer nekoliko izgleda oblika ulaznih labela:")
+print(train_labels[:5])
+
+# Normalizovanje RGB vrednosti slika tako da budu u opsegu od 0 do 1
+train_images, test_images = train_images / 255.0, test_images / 255.0
+
+# Pretvaranje vrednosti labela u jednodimenzionalni niz umesto dvodimenzionalnog
+train_labels = train_labels.reshape(
+    -1,
+)
+
+
+print("\nPrimer jednog kanala ulazne slike:")
+print(train_images[0][0][:3])
+print("\nPrimer nekoliko izgleda oblika ulaznih labela:")
+print(train_labels[:5])
+
+
 num_classes = len(class_names)
 train_images_shape = train_images.shape[1:]
 
-# print(train_images.shape)
-# print(train_images[:5])
 
-# train_labels are unnecessarily two-dimensional - we can reshape it to a single dimension
-# print(train_labels.shape)
-# print(train_labels[:5])
-
-train_labels = train_labels.reshape(-1, )
-# print(train_labels.shape)
-# print(train_labels[:5])
+def sample_plot(images, labels, n):
+    fig, axes = plt.subplots(1, n, figsize=(n * 4, 4))
+    for i in range(n):
+        axes[i].imshow(images[i])
+        label_index = int(labels[i])
+        axes[i].set_xlabel(class_names[label_index], fontsize=13)
 
 
-def sample_plot(images, labels, index):
-    plt.figure(figsize=(4, 4))
-    plt.imshow(images[index])
-    plt.xlabel(class_names[labels[index]], fontsize=13)
-    plt.show()
+sample_plot(train_images, train_labels, 3)
+plt.show()
 
+cnn = models.Sequential(
+    [
+        layers.Conv2D(
+            filters=64,
+            padding="same",
+            kernel_size=(3, 3),
+            activation="relu",
+            input_shape=train_images_shape,
+        ),
+        layers.Dropout(0.2),
+        layers.Conv2D(
+            filters=128, padding="same", kernel_size=(3, 3), activation="relu"
+        ),
+        layers.Dropout(0.2),
+        layers.MaxPooling2D(pool_size=(2, 2)),
+        layers.Conv2D(
+            filters=256, padding="same", kernel_size=(3, 3), activation="relu"
+        ),
+        layers.Dropout(0.2),
+        layers.MaxPooling2D(pool_size=(2, 2)),
+        layers.Flatten(),
+        layers.Dense(512, activation="relu"),
+        layers.Dropout(0.4),
+        layers.Dense(256, activation="relu"),
+        layers.Dropout(0.3),
+        layers.Dense(num_classes, activation="softmax"),
+    ]
+)
 
-# sample_plot(train_images, train_labels, 9)
+cnn.compile(
+    optimizer="adam", loss="sparse_categorical_crossentropy", metrics=["accuracy"]
+)
 
-cnn = models.Sequential([
-    layers.Conv2D(
-        filters=32,
-        kernel_size=(3, 3),
-        activation="relu",
-        input_shape=train_images_shape
-    ),
-    layers.MaxPooling2D(pool_size=(2, 2)),
-    layers.Dropout(0.4),
-
-    layers.Conv2D(
-        filters=64,
-        kernel_size=(3, 3),
-        activation="relu"
-    ),
-    layers.Dropout(0.4),
-    layers.MaxPooling2D(pool_size=(2, 2)),
-
-    layers.Flatten(),
-    layers.Dense(64, activation="relu"),
-    layers.Dropout(0.4),
-    layers.Dense(num_classes, activation="softmax")
-])
-
-cnn.compile(optimizer="adam",
-            loss="sparse_categorical_crossentropy",
-            metrics=["accuracy"])
-
-
-cnn.fit(train_images, train_labels, epochs=10, validation_data=(test_images, test_labels))
-
-# print("\n", "evaluation:")
-# cnn.evaluate(train_images, train_labels)
+cnn.fit(
+    train_images, train_labels, validation_data=(test_images, test_labels), epochs=20
+)
+cnn.evaluate(test_images, test_labels)
